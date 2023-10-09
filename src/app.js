e1c844b7f24ecbaa1c5e05430ef6ae90d910d705
Joshua Permito <dwigoric@insanedevs.com>
2023-10-09 20:41:50 +0800

    feat: configure CORS

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
+// Default FRONTEND_URL
+const DEFAULT_FRONTEND_URL = 'http://localhost:5173'
+
 // Packages
 import express from 'express'
 import cookieParser from 'cookie-parser'
 import logger from 'morgan'
+import cors from 'cors'
 import 'dotenv/config'
 
 // MongoDB
@@ -17,6 +21,17 @@ import usersRouter from './routes/users.js'
 import authRouter from './routes/auth.js'
 
 const app = express()
+
+// Configure CORS
+if (!process.env.FRONTEND_URL)
+    console.warn(`FRONTEND_URL not set, using default: ${DEFAULT_FRONTEND_URL}`)
+
+app.use(
+    cors({
+        origin: process.env.FRONTEND_URL ?? DEFAULT_FRONTEND_URL,
+        optionsSuccessStatus: 200
+    })
+)
 
 // Initialize authentication
 import './auth/auth.js'
