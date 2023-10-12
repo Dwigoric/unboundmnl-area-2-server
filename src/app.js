77e194a5a4ee7a370d7702ab7d62b295994c6f1b
Joshua Permito <dwigoric@insanedevs.com>
2023-10-12 23:09:35 +0800

    feat(app): implement error handler

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@
 const DEFAULT_FRONTEND_URL = 'http://localhost:5173'
 
 // Packages
+import createError from 'http-errors'
 import express from 'express'
 import cookieParser from 'cookie-parser'
 import logger from 'morgan'
@@ -51,5 +52,21 @@ app.use(express.static('public'))
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
 app.use('/auth', authRouter)
+
+// Catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    next(createError(404))
+})
+
+// error handler
+app.use(function (err, req, res) {
+    // set locals, only providing error in development
+    res.locals.message = err.message
+    res.locals.error = req.app.get('env') === 'development' ? err : {}
+
+    // render the error page
+    res.status(err.status || 500)
+    res.send({ message: err.message })
+})
 
 export default app
