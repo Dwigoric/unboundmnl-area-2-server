7651da50d26feac6fccc0032059c618b50db514f
Joshua Permito <dwigoric@insanedevs.com>
2023-11-22 08:10:49 +0800

    !refactor: add region to location schema

--- src/models/locationSchema.js
+++ src/models/locationSchema.js
@@ -1,10 +1,11 @@
 import { Schema } from 'mongoose'
 
 const LocationSchema = new Schema({
-    street: {type: String, required: [true, 'Street is required']},
-    barangay: {type: String, required: [true, 'Barangay is required']},
-    city: {type: String, required: [true, 'City is required']},
-    province: {type: String, required: [true, 'Province is required']}
+    street: { type: String, required: [true, 'Street is required'] },
+    barangay: { type: String, required: [true, 'Barangay is required'] },
+    city: { type: String, required: [true, 'City is required'] },
+    province: { type: String, required: [true, 'Province is required'] },
+    region: { type: String, required: [true, 'Region is required'] }
 })
 
 export default LocationSchema
