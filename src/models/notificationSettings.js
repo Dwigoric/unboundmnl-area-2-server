a6da8e2a26288033512df2aa9b4a8c8b3083bfbc
kndone <justinlim4916@gmail.com>
2023-11-24 23:42:48 +0800

    feat: edit notification settings

--- src/models/notificationSettings.js
+++ src/models/notificationSettings.js
@@ -9,4 +9,19 @@ const NotificationSettingsSchema = new Schema({
 
 const NotificationSettings = new model('NotificationSettings', NotificationSettingsSchema)
 
+// Create default settings
+NotificationSettings.findOne()
+    .lean()
+    .then((existing) => {
+        if (existing) return
+
+        const settings = new NotificationSettings({
+            notification_period_1: 3,
+            notification_period_2: 5,
+            notification_period_3: 7
+        })
+
+        settings.save()
+    })
+
 export default NotificationSettings
